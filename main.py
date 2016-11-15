20704fa0b753c4a4c0329d47641b524daddede66
bdauer <dauer.benjamin@gmail.com>
2016-11-15 13:52:12 -0500

    bug fix

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from app import app, db
+from app import app, database
 from models import *
 from views import *
 
 def main():
-    db.create_tables([Entry, FTSEntry, Comment], safe=True)
+    database.create_tables([Entry, FTSEntry, Comment], safe=True)
     app.run(debug=False)
 
 if __name__ == '__main__':
