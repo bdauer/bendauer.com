161d911e6e3a2c7251c8ad92799ff79b0ce25e93
bdauer <dauer.benjamin@gmail.com>
2016-11-17 11:43:00 -0500

    fix import errors, remove commented out code

--- email_functions/email_comments_interface.py
+++ email_functions/email_comments_interface.py
@@ -6,30 +6,33 @@ from email_settings import (from_email, from_password, smtp_host, port,
 
 from send_email import send_email_via_smtp
 
+from format_email import format_email, format_subject
+
 
 def run_email_update(comment):
     """
-    Checks if conditions for sending comments email have been met.
-    If they have, sends an email.
+    Checks if the new comments queue has reached its threshold.
+    If it has, sends an email.
     """
     updated_comments_length = add_to_recent_comments(comment)
     is_maxed = check_recent_comments_length(updated_comments_length)
 
     if is_maxed:
-        # create and send an email.
         comments, formatted_subject, formatted_email = \
         _build_new_comments_email()
-        # comments = get_recent_comments(comments_file)
-        # formatted_email = format_email(comments)
-        # formatted_subject = format_subject("New comments at bendauer.com")
+
         send_email_via_smtp(from_email, from_password, to_email_for_comments,\
                             formatted_subject, formatted_email, smtp_host, port)
+
         reset_comments_list(comments_file)
 
 
 def send_scheduled_email(comments_file):
     """
-    Used for cronjob. Sends an email containing the comments.
+    Used for cronjob.
+    Sends an email
+    containing the recent comments
+    if there are any.
     """
     recent_comments = get_recent_comments(comments_file)
 
